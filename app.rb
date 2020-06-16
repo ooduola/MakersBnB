3434b4a76c4960ff96b9dc064937cb940dd1e3a4
Rich Ewin <richardewinlambert@gmail.com>
2020-06-16 10:33:09 +0100

    feature test passed - able to view spaces

--- app.rb
+++ app.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 require 'sinatra/base'
 
 class MakersBnB < Sinatra::Base
+  get '/' do
+    @spaces = ['light and airy loft', 'my mums old room', 'sunny basement']
+    erb :index
+  end
 
-get '/' do
-  'hello world'
-end 
-
-run! if app_file == $PROGRAM_NAME
+  run! if app_file == $PROGRAM_NAME
 end
