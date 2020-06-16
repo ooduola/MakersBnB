b483f7d3d76f4abc023fc901ecebac9202c2acbd
Olatunji Oduola <ola.oduola@gmail.com>
2020-06-16 14:39:00 +0100

    refactored to merge with model

--- app.rb
+++ app.rb
@@ -4,21 +4,31 @@ require 'sinatra/base'
 class MakersBnB < Sinatra::Base
   enable :sessions
 
+  # get '/' do
+  #   $spaces = ['light and airy loft', 'my mums old room', 'sunny basement']
+  #   erb :index
+  # end
+
   get '/' do
-    $spaces = ['light and airy loft', 'my mums old room', 'sunny basement']
+    @spaces = MakersBnB.all
     erb :index
   end
+
+  # post '/add_space' do
+  #   session[:space_name] = params[:space_name]
+  #   redirect '/space'
+  # end
 
   post '/add_space' do
-    session[:space_name] = params[:space_name]
-    redirect '/space'
+    MakersBnB.add(params[:space_name])
+    redirect '/'
   end
 
-  get '/space' do
-    $space_name = session[:space_name]
-    $spaces << $space_name
-    erb :index
-  end
+  # get '/space' do
+  #   $space_name = session[:space_name]
+  #   $spaces << $space_name
+  #   erb :index
+  # end
 
   run! if app_file == $PROGRAM_NAME
 end
