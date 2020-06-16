09260554b648f655e0a48a72ed1c60cc34997ad6
Rich Ewin <richardewinlambert@gmail.com>
2020-06-16 11:42:25 +0100

    feature test passed - able to add a space

--- app.rb
+++ app.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
-
 require 'sinatra/base'
 
 class MakersBnB < Sinatra::Base
+  enable :sessions
+
   get '/' do
-    @spaces = ['light and airy loft', 'my mums old room', 'sunny basement']
+    $spaces = ['light and airy loft', 'my mums old room', 'sunny basement']
     erb :index
   end
 
   post '/add_space' do
-    session[:name] = params[:space_name]
-    # redirect '/'
+    session[:space_name] = params[:space_name]
+    redirect '/space'
   end
 
-  # get '/' do
-  #   erb :index
-  # end
+  get '/space' do
+    $space_name = session[:space_name]
+    $spaces << $space_name
+    erb :index
+  end
 
   run! if app_file == $PROGRAM_NAME
 end
