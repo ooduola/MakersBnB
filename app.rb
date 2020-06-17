487c31bf0159f78cbf0a2f1a9b9d2bfc44e8048e
Tim <TimCPB@github.com>
2020-06-17 10:50:40 +0100

    refactored app.rb

--- app.rb
+++ app.rb
@@ -5,31 +5,15 @@ require './lib/makers_bnb'
 class ProjectBnB < Sinatra::Base
   enable :sessions
 
-  # get '/' do
-  #   $spaces = ['light and airy loft', 'my mums old room', 'sunny basement']
-  #   erb :index
-  # end
-
   get '/' do
     @spaces = MakersBnB.all
     erb :index
   end
 
-  # post '/add_space' do
-  #   session[:space_name] = params[:space_name]
-  #   redirect '/space'
-  # end
-
   post '/add_space' do
     MakersBnB.add(params[:space_name])
     redirect '/'
   end
-
-  # get '/space' do
-  #   $space_name = session[:space_name]
-  #   $spaces << $space_name
-  #   erb :index
-  # end
 
   run! if app_file == $PROGRAM_NAME
 end
