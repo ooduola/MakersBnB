f4cdc011dc263a0aee6bb71406422584383fd564
Nick Butler <nwmbutler@gmail.com>
2020-06-17 12:27:35 +0100

    test for username on listings page passing

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,11 +2,9 @@
 feature 'sign up to MakersBnB' do
   scenario 'users can input name and password' do
     visit '/sign_up'
-
     fill_in :username, with: 'ontest'
     fill_in :password, with: '1234'
     click_button 'Submit'
-
     expect(page).to have_current_path('/')
   end
 end
@@ -14,8 +12,17 @@ end
 feature 'sign up page has login button' do
   scenario 'user can see and click login button' do
     visit '/sign_up'
-
     click_button 'Login'
     expect(page).to have_current_path('/login')
+  end
+end
+
+feature 'upon signup listings page contains username' do
+  scenario 'return username on listing page' do
+    visit '/sign_up'
+    fill_in :username, with: 'ontest'
+    fill_in :password, with: '1234'
+    click_button 'Submit'
+    expect(page).to have_content('ontest')
   end
 end
