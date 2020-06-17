8ede534c00022abe748afc4ef0e9109035153b1d
Olatunji Oduola <ola.oduola@gmail.com>
2020-06-17 11:49:37 +0100

    Feature tested login button

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -10,3 +10,12 @@ feature 'sign up to MakersBnB' do
     expect(page).to have_current_path('/')
   end
 end
+
+feature 'sign up page has login button' do
+  scenario 'user can see and click login button' do
+    visit '/sign_up'
+
+    click_button 'Login'
+    expect(page).to have_current_path('/login')
+  end
+end
